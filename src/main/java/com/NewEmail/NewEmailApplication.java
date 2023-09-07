45bd41d1a848e041c6b0c48b50c5ac7ae7ffaff2
Praveen Sagar <praveen.sagar.java@sarvm.ai>
2023-09-07 17:16:53 +0530

    Spring boot email sender SMTP

---
+++ src/main/java/com/NewEmail/NewEmailApplication.java
@@ -0,0 +1,13 @@
+package com.NewEmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NewEmail/NewEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NewEmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
